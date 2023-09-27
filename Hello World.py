951fd4747f12467b38846685c19aa7203af75242
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-09-27 08:37:49 +0800

    sep and end

--- Hello World.py
+++ Hello World.py
@@ -5,5 +5,6 @@ age = int(age)
 pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 # Output
 print(f"Hello, {name}!")
+print("Hello,",name, sep="%%%%", end="!")
 print(f"My age next year is {age + 1}!")
 print(f"Pi value is {pi}!")
