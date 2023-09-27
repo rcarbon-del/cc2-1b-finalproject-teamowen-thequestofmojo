b2f27a87f5eb1fdf1eab0f7e3a168cd38f4a180b
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-09-27 08:33:18 +0800

    final ata

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,9 @@
+# Variables and Input
 name = input("What is your name?")
 age = input("What is your age?")
 age = int(age)
-
+pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+# Output
 print(f"Hello, {name}!")
 print(f"My age next year is {age + 1}!")
+print(f"Pi value is {pi}!")
