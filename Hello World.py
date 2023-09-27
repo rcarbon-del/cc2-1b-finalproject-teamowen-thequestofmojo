a6f64a91ab4bd16576d7268f66cb5d437fef7599
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-09-27 08:27:03 +0800

    convert int

--- Hello World.py
+++ Hello World.py
@@ -1,5 +1,6 @@
 name = input("What is your name?")
 age = input("What is your age?")
+age = int(age)
 
 print(f"Hello, {name}!")
-print(f"My age next year is {int(age) + 1}!")
+print(f"My age next year is {age + 1}!")
