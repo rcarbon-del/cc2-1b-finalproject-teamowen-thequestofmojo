cf065cf5ccdecf62e9b0d8f6b31bc68f7e18c60f
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-09-28 07:39:44 +0800

    FINAL COMMIT ```

--- labact2-hello-world/Hello World.py
+++ labact2-hello-world/Hello World.py
@@ -6,7 +6,10 @@ pi = 3.141592653589793
 modulo = 5 % 3
 # Output
 print(f"Hello, {name}!")
+print("Hello,", name)
+print("Hello, "+name)
 print("Hello,",name, sep="%%%%")
 print(f"My age next year is {age + 1}!")
 print(f"Pi value is {pi}!")
 print(f"Modulo is {modulo}!")
+print(f"Modulo is {5%3}!")
