3ac341219037d994d39cbdaf836beb24492bbfdf
rcarbon <carbzzradge@gmail.com>
2023-10-12 20:00:16 +0800

    modified:   labact3-loops/right_triangle.py

--- labact3-loops/right_triangle.py
+++ labact3-loops/right_triangle.py
@@ -1,1 +1,9 @@
 # Right Triangle 
+
+rows = 1 # starting amount of rows
+final_rows = 5 # final amount of rows
+while rows <= final_rows: 
+  for i in range(1, rows+1): 
+    print(i, end=" ") 
+  print()
+  rows += 1
