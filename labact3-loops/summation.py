b9948655a870d0c7e49f50ef3c7bf52ae921f8aa
Radge <rac4843@students.uc-bcf.edu.ph>
2023-10-12 20:50:54 +0800

    FINAL COMMIT (added descriptions/comments to the code)

--- labact3-loops/summation.py
+++ labact3-loops/summation.py
@@ -9,12 +9,12 @@ formula_list = []
 # Formula
 while len(formula_list) < input_number:
     for i in range(1, input_number+1):
-        formula_list.append(i)
-print("Formula:", "+".join(map(str, formula_list)))
+        formula_list.append(i) # add the numbers to the list
+print("Formula:", "+".join(map(str, formula_list))) # make the formula list into a string and join them with a plus sign
 
 # Summation
-for i in range(1, input_number+1):
-    sum += i
+for i in range(1, input_number+1): # loop the numbers from 1 to the input number
+    sum += i # add the numbers to the sum
 print("Summation:", sum)
 # End of program
 
