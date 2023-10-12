bc24ffa1d6bc33d1c1b5fb0a135b99bea7180248
Radge <rac4843@students.uc-bcf.edu.ph>
2023-10-12 20:12:19 +0800

    FINAL

---
+++ labact3-loops/inverse_right_triangle.py
@@ -0,0 +1,10 @@
+# Inverse Right Traingle
+
+rows = 5 # starting amount of rows
+final_rows = 1 # final amount of rows
+while rows >= final_rows: 
+  for i in range(1, rows+1): 
+    print(i, end=" ") 
+  print()
+  rows -= 1
+  # End of program
