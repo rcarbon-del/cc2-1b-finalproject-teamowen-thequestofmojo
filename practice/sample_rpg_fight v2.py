2cb9c15ccb7dba62a3851fc1675a55b05eb39199
Radge <rac4843@students.uc-bcf.edu.ph>
2023-10-20 10:36:20 +0800

    new features

--- practice/sample_rpg_fight v2.py
+++ practice/sample_rpg_fight v2.py
@@ -1,44 +1,86 @@
 import random
+import os
+from turtle import clearscreen
 
 bossHealth = random.randint(1500, 2000)
 health = random.randint(1000, 1500)
-
+weaponDamage = 50
+healCounter = 3
 while bossHealth or health == True:
     if health >= 0:
-        dmg = random.randint(10, 200)
-        bossHealth -= dmg
-        if bossHealth <= 0:
-            bossHealth = 0
-            print("You dealt", dmg, "damage to the boss!")
-            print("The boss has", bossHealth, "health left!")
-            print()
-            break
-        else:
-            if dmg >= 150:
-                print("Critical hit!")
-            print("You dealt", dmg, "damage to the boss!")
-            print("The boss has", bossHealth, "health left!")
-            print()
+        print("What will you do?")
+        print("1. Attack")
+        print(f"2. Heal ({healCounter} left)")
+        print("3. Run")
+        print()
+        choice = input("Enter choice: ")
+        print()
+        if choice == "1": 
+            dmg = random.randint(weaponDamage, weaponDamage+200)
+            bossHealth -= dmg
+            if bossHealth <= 0:
+                bossHealth = 0
+                print("You dealt", dmg, "damage to the boss!")
+                print("The boss has", bossHealth, "health left!")
+                print()
+                break
+            else:
+                if dmg >= 150:
+                    print("Critical hit!")
+                print("You dealt", dmg, "damage to the boss!")
+                print("The boss has", bossHealth, "health left!")
+                print()
+            
+            if bossHealth >= 0:
+                bossdmg = random.randint(10, 100)
+                health -= bossdmg
+                if health <= 0:
+                    health = 0
+                    print("The boss dealt", bossdmg, "damage to you!")
+                    print("You have", health, "health left!")
+                    print()
+                    break
+                else:
+                    print("The boss dealt", bossdmg, "damage to you!")
+                    if bossdmg >= 75:
+                        print("The attack was super effective!")
+                    print("You have", health, "health left!")
+                    print()
+            continueInput = input("Press enter to continue...")
+            os.system('cls' if os.name == 'nt' else 'clear')
 
-    if bossHealth >= 0:
-        bossdmg = random.randint(10, 100)
-        health -= bossdmg
-        if health <= 0:
-            health = 0
-            print("The boss dealt", bossdmg, "damage to you!")
-            print("You have", health, "health left!")
-            print()
+        elif choice == "2":
+            if healCounter > 0:
+                heal = random.randint(100, 200)
+                health += heal
+                if health >= 1500:
+                    health = 1500
+                    print("You healed", heal, "health!")
+                    print("You have", health, "health left!")
+                    print()
+                else:
+                    print("You healed", heal, "health!")
+                    print("You have", health, "health left!")
+                    print()
+                healCounter -= 1                
+            else:
+                print("You have no more heals left!")
+                print()
+            continueInput = input("Press enter to continue...")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
+        elif choice == "3":
+            print("You ran away!")
             break
-        else:
-            if bossdmg >= 75:
-                print("The attack was super effective!")
-            print("The boss dealt", bossdmg, "damage to you!")
-            print("You have", health, "health left!")
+        else:  
+            print("Invalid input!")
             print()
+        
 
 print("Game over!")
 if health == 0:
         print("You died!")
 elif bossHealth == 0:
         print("You won!")
+print()
         
