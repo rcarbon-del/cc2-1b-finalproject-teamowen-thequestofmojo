d289751ac3a33e45a594424fad2fdecc4b4e479a
Radge <rac4843@students.uc-bcf.edu.ph>
2023-10-20 08:45:21 +0800

    FINISHED ```

--- practice/sample_rpg_fight.py
+++ practice/sample_rpg_fight.py
@@ -4,26 +4,37 @@ bossHealth = random.randint(1500, 2000)
 health = random.randint(500, 1000)
 
 while bossHealth or health == True:
-    if health <= 0:
+    if health >= 0:
         dmg = random.randint(10, 200)
         bossHealth -= dmg
-        if health <= 0:
-            health = 0
-            break
-        print("You dealt", dmg, "damage to the boss!")
-        print("The boss has", bossHealth, "health left!")
-
-    if bossHealth <= 0:
-        bossdmg = random.randint(10, 50)
-        health -= bossdmg
         if bossHealth <= 0:
             bossHealth = 0
+            print("You dealt", dmg, "damage to the boss!")
+            print("The boss has", bossHealth, "health left!")
+            print()
             break
-        print("The boss dealt", bossdmg, "damage to you!")
-        print("You have", health, "health left!")
+        else:
+            print("You dealt", dmg, "damage to the boss!")
+            print("The boss has", bossHealth, "health left!")
+            print()
 
+    if bossHealth >= 0:
+        bossdmg = random.randint(10, 50)
+        health -= bossdmg
+        if health <= 0:
+            health = 0
+            print("The boss dealt", bossdmg, "damage to you!")
+            print("You have", health, "health left!")
+            print()
+            break
+        else:
+            print("The boss dealt", bossdmg, "damage to you!")
+            print("You have", health, "health left!")
+            print()
+
+print("Game over!")
 if health == 0:
         print("You died!")
-if bossHealth == 0:
+elif bossHealth == 0:
         print("You won!")
         
