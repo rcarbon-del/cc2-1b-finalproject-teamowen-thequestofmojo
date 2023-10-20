1469609edc61bbaee75b8d450d8a3ddefe6494dc
Radge <rac4843@students.uc-bcf.edu.ph>
2023-10-20 13:53:21 +0800

    final

--- practice/sample_rpg_fight v2.py
+++ practice/sample_rpg_fight v2.py
@@ -1,9 +1,15 @@
 import random
 
 bossHealth = random.randint(1500, 2000)
-health = random.randint(1000, 1500)
+health = 700
 initialWeaponDamage = 50
 healCounter = 3
+
+def clearOutput(num_lines: int):
+    cursorUpOne = '\x1b[1A'
+    erase = '\x1b[2K'
+    for _ in range(num_lines):
+        print(cursorUpOne + erase, end='')
 
 while bossHealth or health == True:
     if health >= 0:
@@ -24,15 +30,15 @@ while bossHealth or health == True:
                 print()
                 break
             else:
-                if dmg >= 150:
+                if dmg >= 175:
                     print("Critical hit!")
                 print("You dealt", dmg, "damage to the boss!")
                 print("The boss has", bossHealth, "health left!")
                 print()
             
+            bossdmg = random.randint(10, 100)
+            health -= bossdmg
             if bossHealth >= 0:
-                bossdmg = random.randint(10, 100)
-                health -= bossdmg
                 if health <= 0:
                     health = 0
                     print("The boss dealt", bossdmg, "damage to you!")
@@ -45,35 +51,44 @@ while bossHealth or health == True:
                         print("The attack was super effective!")
                     print("You have", health, "health left!")
                     print()
+            end = input("Press enter to continue...")
+            clearOutput(14)
+            if dmg >= 175:
+                clearOutput(1)
+            if bossdmg >= 75:
+                clearOutput(1)
 
         elif choice == "2":
             if healCounter > 0:
-                heal = random.randint(100, 200)
+                heal = random.randint(75, 125)
                 health += heal
-                if health >= 1500:
-                    health = 1500
+                if health >= 700:
+                    health = 700
                     print("You healed", heal, "health!")
-                    print("You have", health, "health left!")
+                    print("You have max left!")
                     print()
                 else:
                     print("You healed", heal, "health!")
                     print("You have", health, "health left!")
                     print()
-                healCounter -= 1                
+                healCounter -= 1
+                end = input("Press enter to continue...")     
+                clearOutput(11)           
             else:
                 print("You have no more heals left!")
                 print()
-            
+                end = input("Press enter to continue...")
+                clearOutput(10)
+
         elif choice == "3":
             print("You ran away!")
             break
         else:  
-            print("Invalid input!")
-            print()
-        
+            clearOutput(7)
 
-print("Game over!")
 if health == 0:
+        print("Game over!")
+        print()
         print("You died!")
 elif bossHealth == 0:
         print("You won!")
